d34c0187a0d4015283fce551fb8b701f3ce7242d
Jaclyn Baughman <jaclynbaughman@Jaclyns-MacBook-Pro-2.local>
2021-09-24 16:14:25 -0700

    make bread calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import { useState } from "react";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import "./App.css"
 import crumbs from './crumbs.jpg';
 
-function sourmath(flour, hydration, leaven){
-var leaven_half = leaven/2 //leaven is equal parts water and flour
-var flour_total = (flour + leaven_half).toFixed(0)
+function sourmath(flour, hydration, leaven, poolish, biga, ratioStarter, ratioBiga){
+  let leaven_flour = leaven/2 
+  let leaven_water = leaven/2 
+  let biga_flour
+  let biga_water
+  if (ratioStarter == "2:1") {
+    leaven_flour = leaven*(2/3) 
+    leaven_water = leaven*(1/3)
+  } else if (ratioStarter == "3:2") {
+    leaven_flour = leaven*(0.6)
+    leaven_water = leaven*(0.4)
+  } 
+  if (ratioBiga == "3:2") {
+    biga_flour = biga*(0.6) 
+    biga_water = biga*(0.4)
+  } else {
+    biga_flour = biga*(2/3) 
+    biga_water = biga*(1/3)
+  }
+
+let poolish_half = poolish/2 //poolish is equal parts water and flour
+let flour_total = (flour + leaven_flour + poolish_half + biga_flour).toFixed(0)
 let salt = (flour_total * .02).toFixed(0)
-let water = ((flour_total*(hydration/100)) - leaven_half).toFixed(0)
+let water = ((flour_total*(hydration/100)) - (leaven_water + poolish_half + biga_water)).toFixed(0)
 let total_water = (flour_total*(hydration/100)).toFixed(0)
 
 
 let flour_percent = (flour/flour * 100)
 let salt_percent = (salt/flour_total * 100).toFixed(0)
-let leaven_perecent = (leaven_half/flour_total * 100).toFixed(0)
+let leaven_perecent = (leaven_flour/flour_total * 100).toFixed(0)
+let poolish_percent = (poolish_half/flour_total * 100).toFixed(0)
+let biga_percent = (biga_flour/flour_total * 100).toFixed(0)
 let water_percent = (water/flour_total * 100).toFixed(0)
 let total_water_percent = (total_water/flour_total * 100).toFixed(0)
 
 let data_table = {total_flour: [`${flour_total} g`, `${flour_percent}%`], 
                   salt: [`${salt} g`, `${salt_percent}%`], 
-                  leaven: [`${leaven} g`, `${leaven_perecent}%`], 
+                  leaven: [`${leaven} g`, `${leaven_perecent}%`],
+                  poolish: [`${poolish} g`, `${poolish_percent}%`],
+                  biga: [`${biga} g`, `${biga_percent}%`],
                   water: [`${water} g`, `${water_percent}%`], 
                   total_water: [`${total_water} g`, `${total_water_percent}%`]}
 
@@ -30,14 +55,18 @@ export default function App() {
     const [flour, setFlour] = useState(200);
     const [hydration, setHydration] = useState(10);
     const [leaven, setLeaven] = useState(0);
-    const bread_table = sourmath(flour, hydration, leaven);
-    
+    const [poolish, setPoolish] = useState(0);
+    const [biga, setBiga] = useState(0);
+    const [ratioStarter, setRatioStarter] = useState(null);
+    const [ratioBiga, setRatioBiga] = useState(null);
+    const bread_table = sourmath(flour, hydration, leaven, poolish, biga, ratioStarter, ratioBiga);
+    const ratio_options = ["1:1", "3:2", "2:1"];
     return (
       <div className="App">       
         <h1>Baker's Percentage Calculator</h1>
-        <img src={crumbs} height={600} width={600}/> 
+        <img src={crumbs} width={500}/> 
         <h3> A Web App by <a href="http://jclyn.info">Jaclyn Baughman</a> </h3> 
-        <p> 🍞 Let's make bread! All we need is four ingredients and some arithmetic. To use this calculator, choose how much flour you want to use, your hydration target, and your amount of preferment.</p>
+        <p> 🍞 Let's make bread! All we need is flour, water, salt, and yeast, plus some arithmetic. To use this calculator, choose how much flour you want to use, your hydration target, and your amount of preferment.</p>
         <h2>Calculator</h2>
         <div className="input-group">
           <label for="flourSlider">Flour (g)</label>
@@ -86,7 +115,7 @@ export default function App() {
         />
         </div>
         <div className="input-group">
-        <label for="leavenSlider">Leaven/Starter/Poolish (g) (1:1 ratio flour and water)</label>
+        <label for="leavenSlider">Leaven/Starter (g) (flour, water, natural yeast)</label>
           <input
             id="leavenSlider"
             type="number"
@@ -97,6 +126,12 @@ export default function App() {
               setLeaven(e.target.valueAsNumber);
             }}
           />
+          <Dropdown
+           options={ratio_options}
+           placeholder="Choose your ratio of flour:water (standard is 1:1)"
+           value={ratioStarter}
+           onChange={(options) => setRatioStarter(options.value)}
+        />
           <input
           id="leavenRange"
           type="range"
@@ -108,12 +143,66 @@ export default function App() {
           }}
         />
         </div>
+        <div className="input-group">
+        <label for="poolishSlider">Poolish (g) (1:1 ratio of flour and water + commercial yeast) </label>
+          <input
+            id="poolishSlider"
+            type="number"
+            min={0}
+            max={500}
+            value={poolish}
+            onChange={(e) => {
+              setPoolish(e.target.valueAsNumber);
+            }}
+          />
+          <input
+          id="poolishRange"
+          type="range"
+          min={0}
+          max={500}
+          value={poolish}
+          onChange={(e) => {
+            setPoolish(e.target.valueAsNumber);
+          }}
+          />
+        </div>
+        <div className="input-group">
+        <label for="bigaSlider">Biga (g) (flour, water, commercial yeast) </label>
+          <input
+            id="bigaSlider"
+            type="number"
+            min={0}
+            max={1500}
+            value={biga}
+            onChange={(e) => {
+              setBiga(e.target.valueAsNumber);
+            }}
+          />
+          <Dropdown
+          options={ratio_options}
+          placeholder="Choose your ratio of flour:water (standard is 2:1)"
+          value={ratioBiga}
+          onChange={(options) => setRatioBiga(options.value)}
+        />
+          <input
+          id="poolishBiga"
+          type="range"
+          min={0}
+          max={1500}
+          value={biga}
+          onChange={(e) => {
+            setBiga(e.target.valueAsNumber);
+          }}
+          />
+        </div>
         <table>
           <tr>
             <td></td>
             <td>Total Flour</td>
             <td>Salt</td>
-            <td>Preferment</td>
+            <td>Leaven</td>
+            <td>Poolish</td>
+            <td>Biga</td>
             <td>Water</td>
             <td>Total Water</td>
           </tr>
@@ -122,6 +211,8 @@ export default function App() {
             <td>{bread_table.total_flour[0]}</td>
             <td>{bread_table.salt[0]}</td>
             <td>{bread_table.leaven[0]}</td>
+            <td>{bread_table.poolish[0]}</td>
+            <td>{bread_table.biga[0]}</td>
             <td>{bread_table.water[0]}</td>
             <td>{bread_table.total_water[0]}</td>
           </tr>
@@ -130,12 +221,14 @@ export default function App() {
             <td>{bread_table.total_flour[1]}</td>
             <td>{bread_table.salt[1]}</td>
             <td>{bread_table.leaven[1]}</td>
+            <td>{bread_table.poolish[1]}</td>
+            <td>{bread_table.biga[1]}</td>
             <td>{bread_table.water[1]}</td>
             <td>{bread_table.total_water[1]}</td>
           </tr>
         </table>
         <ul>
-        <li> The percentage for the leaven/starter/poolish is the amount of flour in the preferment mixture divided by the total flour in the recipe. If you aren't using any, put 0!</li>
+        <li> The percentage for the leaven/starter/poolish/biga is the amount of flour in the preferment mixture divided by the total flour in the recipe. If you aren't using any, put 0!</li>
         </ul>
         <h2> Bread Notes </h2>
         <ul>
@@ -150,5 +243,4 @@ export default function App() {
         </ul>
       </div>
     );
-  }
-  
+    }
